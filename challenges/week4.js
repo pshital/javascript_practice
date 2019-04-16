40ffb4a864266aa857b1130d4f2435a540e693dd
pshital <skpawar@gmail.com>
2019-04-16 23:41:51 +0100

    week4

--- challenges/week4.js
+++ challenges/week4.js
@@ -1,43 +1,97 @@
 function findSmallNums(nums) {
   if (!nums) throw new Error("nums is required");
-  // Your code here
+  let small = []
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] < 1)
+      small.push(nums[i])
+  }
+  /*arr.forEach(function(item){
+    if(item<1)
+    small.push(item)
+  })
+  console.log(small)
+  const smallerThan1= arr.map(function(num){
+  if (num<1)
+  return num
+  else
+  return 
+  })*/
+  return small
 }
 
 function findNamesBeginningWith(names, char) {
   if (!names) throw new Error("names is required");
   if (!char) throw new Error("char is required");
-  // Your code here
+  let newNames = []
+  names.forEach(element => {
+    if (element.charAt(0) == char)
+      newNames.push(element)
+  });
+  return newNames
 }
 
 function findVerbs(words) {
   if (!words) throw new Error("words is required");
-  // Your code here
+  let verb = []
+  for (let i = 0; i < words.length; i++) {
+    let newarray = words[i].split(' ')
+    if (newarray[0]=="to")
+      verb.push(words[i])
+  }
+  return verb
 }
 
 function getIntegers(nums) {
   if (!nums) throw new Error("nums is required");
-  // Your code here
+  let integerArray = []
+  for (let i = 0; i < nums.length; i++) {
+    if (Number.isInteger(nums[i]))
+      integerArray.push(nums[i])
+  }
+  return integerArray
 }
 
 function getCities(users) {
   if (!users) throw new Error("users is required");
-  // Your code here
+  let arrayofCities = []
+  for (let i = 0; i < users.length; i++) {
+    arrayofCities.push(users[i].data.city.displayName)
+  }
+  return arrayofCities
 }
 
 function getSquareRoots(nums) {
   if (!nums) throw new Error("nums is required");
-  // Your code here
+  let squareArray = []
+  for (let i = 0; i < nums.length; i++) {
+    {
+      squareArray[i] = (Math.sqrt(nums[i]).toPrecision(3))
+      //squareArray[i] = parseFloat(squareArray[i])
+     //squareArray[i]= Number(squareArray[i])
+    }
+  }
+  return(squareArray)
 }
 
 function findSentencesContaining(sentences, str) {
   if (!sentences) throw new Error("sentences is required");
   if (!str) throw new Error("str is required");
-  // Your code here
+  let specificStr = []
+  sentences.forEach(function (line) {
+    let line1 = line.toLowerCase()
+    if (line1.includes(str))
+      specificStr.push(line)
+
+  })
+  return specificStr
 }
 
 function getLongestSides(triangles) {
   if (!triangles) throw new Error("triangles is required");
-  // Your code here
+  let longerSide = []
+  for (let i = 0; i < triangles.length; i++)
+    longerSide.push(Math.max(...triangles[i]))
+  return longerSide
 }
 
 module.exports = {
--- test/week4.test.js
+++ test/week4.test.js
@@ -114,7 +114,7 @@ describe("getCities", () => {
 describe("getSquareRoots", () => {
   test("gets the square root of each number to 2 decimal places", () => {
     const nums = [36, 77, 12, 355, 92, 5];
-    expect(getSquareRoots(nums)).toEqual([6, 8.77, 3.46, 18.84, 9.59, 2.24]);
+    expect(getSquareRoots(nums)).toEqual([6.00, 8.77, 3.46, 18.84, 9.59, 2.24]);
   });
 });
 
